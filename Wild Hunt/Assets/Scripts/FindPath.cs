c706012e67012050b5e9aaf147e0a2a89c0b00fe
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-11-17 05:56:44 +0900

    [add]フィールド変数追加

--- Wild Hunt/Assets/Scripts/FindPath.cs
+++ Wild Hunt/Assets/Scripts/FindPath.cs
@@ -2,15 +2,14 @@ using UnityEngine;
 
 public class FindPath : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    /// <summary>パスの最大登録数</summary>
+    const int _maxPositions = 9;
+    /// <summary>開始地点</summary>
+    [SerializeField, Header("Start")]
+    private Transform _startPos;
+    /// <summary>到達地点</summary>
+    [SerializeField, Header("End")]
+    private Transform _endPos;
+    /// <summary>開始地点から到達地点までの座標の配列</summary>
+    private Vector3[] _positions = new Vector3[_maxPositions];
 }
