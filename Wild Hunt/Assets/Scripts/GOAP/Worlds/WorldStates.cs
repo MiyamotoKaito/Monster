1c5b1d133a1bce49fc538491532c20ea13d68568
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-11-17 18:30:15 +0900

    [add]ステートを持っているかどうかの戻り値boolの関数

--- Wild Hunt/Assets/Scripts/GOAP/Worlds/WorldStates.cs
+++ Wild Hunt/Assets/Scripts/GOAP/Worlds/WorldStates.cs
@@ -11,8 +11,15 @@ public class WorldState
 public class WorldStates
 {
     public Dictionary<string, int> _states;
+    //変数初期化用のコンストラクタ
     public WorldStates()
     {
         _states = new Dictionary<string, int>();
     }
+
+    public bool HasState(string key)
+    {
+        return _states.ContainsKey(key);
+    }
+
 }
