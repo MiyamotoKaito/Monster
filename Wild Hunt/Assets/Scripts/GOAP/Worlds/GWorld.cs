89847e08250c8ca2e8d48d101a2db74b369849a1
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-11-18 16:55:29 +0900

    [add]サマリー追加

--- Wild Hunt/Assets/Scripts/GOAP/Worlds/GWorld.cs
+++ Wild Hunt/Assets/Scripts/GOAP/Worlds/GWorld.cs
@@ -1,13 +1,13 @@
-﻿using UnityEngine;
-
-/// <summary>
+﻿/// <summary>
 /// グローバルワールドステート管理クラス
 /// </summary>
 public class GWorld
 {
+    //シングルトンインスタンス
     private static readonly GWorld instanse = new GWorld();
+    //グローバルワールドステート
     private static WorldStates _world;
-
+    //静的コンストラクタ
     static GWorld()
     {
         _world = new WorldStates();
@@ -18,10 +18,15 @@ public class GWorld
     private GWorld()
     {
     }
+    //シングルトンインスタンスの取得
     public static GWorld Instance
     {
         get { return instanse; }
     }
+    /// <summary>
+    /// グローバルワールドステートの取得
+    /// </summary>
+    /// <returns></returns>
     public WorldStates GetWorld()
     {
         return _world;
