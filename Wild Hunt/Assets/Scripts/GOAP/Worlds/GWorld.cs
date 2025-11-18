93807d959661dda0a37d90a8b94c070af578c3ba
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-11-18 14:47:48 +0900

    [add]ワールドステートを管理する処理追加

--- Wild Hunt/Assets/Scripts/GOAP/Worlds/GWorld.cs
+++ Wild Hunt/Assets/Scripts/GOAP/Worlds/GWorld.cs
@@ -1,6 +1,29 @@
-using UnityEngine;
+﻿using UnityEngine;
 
+/// <summary>
+/// グローバルワールドステート管理クラス
+/// </summary>
 public class GWorld
 {
     private static readonly GWorld instanse = new GWorld();
+    private static WorldStates _world;
+
+    static GWorld()
+    {
+        _world = new WorldStates();
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    private GWorld()
+    {
+    }
+    public static GWorld Instance
+    {
+        get { return instanse; }
+    }
+    public WorldStates GetWorld()
+    {
+        return _world;
+    }
 }
