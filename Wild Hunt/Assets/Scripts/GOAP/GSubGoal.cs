30f1c4f624474b9f79e04e1f7feb8505266e60d4
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-01-01 22:25:26 +0900

    [add]ターゲットの変数作成

--- Wild Hunt/Assets/Scripts/GOAP/GSubGoal.cs
+++ Wild Hunt/Assets/Scripts/GOAP/GSubGoal.cs
@@ -7,11 +7,14 @@ public class GSubGoal
     public string Name;
     public bool Remove;
     public GameObject TargetObj;
-    public GSubGoal(string goalName, int priority, bool remove, GameObject target)
+    public string TargetTag;
+
+    public GSubGoal(string goalName, int priority, bool remove, GameObject target, string targetTag = null)
     {
         SubGoals = new Dictionary<string, int>();
         Name = goalName;
         TargetObj = target;
+        TargetTag = targetTag;
         SubGoals.Add(goalName, priority);
         Remove = remove;
     }
