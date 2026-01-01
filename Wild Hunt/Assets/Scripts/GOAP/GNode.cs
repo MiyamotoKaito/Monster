4f9462389579d48f732118cdd4e04147ae00ae4c
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-01-01 22:25:01 +0900

    [delete]Using削除

--- Wild Hunt/Assets/Scripts/GOAP/GNode.cs
+++ Wild Hunt/Assets/Scripts/GOAP/GNode.cs
@@ -1,7 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using UnityEditor.Experimental.GraphView;
-using UnityEngine;
 
 public class GNode
 {
@@ -9,7 +6,7 @@ public class GNode
     public int Cost;
     public Dictionary<string, int> State;
     public IAction Action;
-    
+
     public GNode(GNode parent, int cost, Dictionary<string, int> allState, IAction action)
     {
         Parent = parent;
